a898674699c0400d5606df4b80a02a469a78eb7f
Luke Hayes <lukehayes2021@u.northwestern.edu>
2019-08-02 18:49:12 -0500

    Portfolio Update

--- animate.js
+++ animate.js
@@ -13,12 +13,12 @@ document.addEventListener('DOMContentLoaded',function(event){
         // wait for a while and call this function again for next character
         setTimeout(function() {
           typeWriter(text, i + 1, fnCallback)
-        }, 200);
+        }, 150);
       }
       // text finished, call callback if there is a callback function
       else if (typeof fnCallback == 'function') {
         // call callback after timeout
-        setTimeout(fnCallback, 800);
+        setTimeout(fnCallback, 700);
       }
     }
     // start a typewriter animation for a text in the dataText array
@@ -26,7 +26,7 @@ document.addEventListener('DOMContentLoaded',function(event){
        if (typeof dataText[i] == 'undefined'){
           setTimeout(function() {
             StartTextAnimation(0);
-          }, 1700);
+          }, 1300);
        }
        // check if dataText[i] exists
       if (i < dataText[i].length) {
